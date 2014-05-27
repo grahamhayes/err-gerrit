54c9119395d427661dab8e297dc71b54e3c33fc4
Graham Hayes <graham.hayes@hp.com>
2014-05-27 01:50:34 +0100

    testing stuff

---
+++ gerrit.py
@@ -0,0 +1,48 @@
+# This is a skeleton for Err plugins, use this to get started quickly.
+
+from errbot import BotPlugin, botcmd
+from pygerrit.client import GerritClient
+import time
+
+
+class Gerrit(BotPlugin):
+    """An Err plugin skeleton"""
+    min_err_version = '1.6.0'  # Optional, but recommended
+    max_err_version = '2.0.0'  # Optional, but recommended
+
+    def activate(self):
+        """Triggers on plugin activation
+
+        You should delete it if you're not using it to
+        override any default behaviour"""
+        super(Gerrit, self).activate()
+        self.client = GerritClient("gerritssh")
+        self.client.start_event_stream()
+
+    def deactivate(self):
+        """Triggers on plugin deactivation
+
+        You should delete it if you're not using it to
+        override any default behaviour"""
+        super(Gerrit, self).deactivate()
+        self.client.stop_event_stream()
+
+    def callback_connect(self):
+        """Triggers when bot is connected
+
+        You should delete it if you're not using it to
+        override any default behaviour"""
+
+        while True:
+            event = self.client.get_event()
+            if event:
+                yield ("Event: %s", event)
+            else:
+                time.sleep(1)
+
+    # Passing split_args_with=None will cause arguments to be split on any kind
+    # of whitespace, just like Python's split() does
+    @botcmd(split_args_with=None)
+    def example(self, mess, args):
+        """A command which simply returns 'Example'"""
+        return "Example"
